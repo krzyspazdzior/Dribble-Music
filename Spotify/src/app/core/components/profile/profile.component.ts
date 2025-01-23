683b1f7e4f584347192416e6ba3051d762736a33
Krzysztof <1p22pazdzior@gmail.com>
2025-01-23 15:11:38 +0100

    poprawki frontendowe

--- Spotify/src/app/core/components/profile/profile.component.ts
+++ Spotify/src/app/core/components/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { Profile } from '../../models/profile.model'
 import { NgIf } from '@angular/common';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
@@ -14,16 +15,26 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class ProfileComponent implements OnInit {
   userProfile: Profile | null = null;
   errorMessage: string = '';
+  isLoading: boolean = true; 
 
   constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
+    const token = localStorage.getItem('spotify_token');
+    
+    if (!token && localStorage.getItem('access_token') == null) {
+      this.userProfile = null;
+      return;
+    }
+    
     this.profileService.getUserProfile().subscribe({
       next: (profileData: Profile) => {
         this.userProfile = profileData;
+        this.isLoading = false;
       },
       error: (error) => {
         this.errorMessage = 'An error occurred while fetching user data.';
+        this.isLoading = false;
         console.error('Error fetching user data:', error);
       }
     });
