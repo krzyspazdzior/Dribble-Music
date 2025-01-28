610fb8254aadb67bc4d4fd88e57f49f7bc7cfa93
Krzysztof <1p22pazdzior@gmail.com>
2025-01-28 13:36:11 +0100

    strona glowna

--- Spotify/src/app/core/components/home/home.component.ts
+++ Spotify/src/app/core/components/home/home.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {}
 }
